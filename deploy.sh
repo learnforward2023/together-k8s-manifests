529462cc182dd6397f4bd9bb861ff4435991cba6
Learn Forward 2023 <learnforward2023@gmail.com>
2024-03-09 21:27:54 +0900

    fix: pull latest changes before apply k8s

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,11 @@
 echo "Deploy process started"
 
+# cd to the project directory
+cd ~/together-k8s-manifests
+
+# Pull latest changes
+git pull origin main
+
 # Apply deployment and service
 kubectl apply -f ./services/project-web
 
